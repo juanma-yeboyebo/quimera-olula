cf1056479140fae444beefeca37952285520b0d9
Javier-Valero-yeboyebo <jvaola@gmail.com>
2026-04-08 12:34:18 +0200

    refact(SAN): Añadidas opciones de menu de ususario.. Permisos PENDIENTE.

--- apps/sanhigia/src/factory.ts
+++ apps/sanhigia/src/factory.ts
@@ -4,6 +4,8 @@ import { ExtraLogoSanhigia } from "./componentes/ExtraLogoSanhigia";
 import { MenuUsuarioSanhigia } from "./componentes/MenuUsuarioSanhigia";
 import { FactoryAlmacenLegacy } from "./contextos/almacen/factory.ts";
 import { FactoryInformesLegacy } from "./contextos/informes/factory.ts";
+import { menuUsuarioSanhigia } from "./contextos/menu";
+import { procesarElementosSanhigia } from "./contextos/procesarMenuUsuario";
 import { FactorySmartsalesLegacy } from "./contextos/smartsales/factory.ts";
 import { FactoryVentasLegacy } from "./contextos/ventas/factory.ts";
 
@@ -12,6 +14,8 @@ class FactoryComponentesSanhigia {
     // static cabecera_acciones = AccionesCabeceraSanhigia;
     static cabecera_menu_usuario = MenuUsuarioSanhigia;
     static cabecera_extra_logo = ExtraLogoSanhigia;
+    static menu_usuario_elementos = menuUsuarioSanhigia;
+    static menu_usuario_procesar_elementos = procesarElementosSanhigia;
 }
 
 export class FactoryLegacy {
