9b37bc00ae9ba476d0a46050b8b74537130a31ae
xavikortes <javiercortesveliz@gmail.com>
2026-04-21 11:13:50 +0200

    feat. mejoras estilo tarjeta generica

--- packages/componentes/src/moleculas/qtarjeta_generica.tsx
+++ packages/componentes/src/moleculas/qtarjeta_generica.tsx
@@ -5,20 +5,20 @@ import "./qtarjeta_generica.css";
 type QTarjetaGenericaProps = {
   mostrarTodo?: boolean;
   avatar?: React.ReactNode;
-  arribaIzquierda?: React.ReactNode;
-  arribaDerecha?: React.ReactNode;
-  abajoIzquierda?: React.ReactNode;
-  abajoDerecha?: React.ReactNode;
+  arribaIzquierda?: string | React.ReactNode;
+  arribaDerecha?: string | React.ReactNode;
+  abajoIzquierda?: string | React.ReactNode;
+  abajoDerecha?: string | React.ReactNode;
   expansion?: React.ReactNode;
 };
 
 export const QTarjetaGenerica = ({
   mostrarTodo = false,
   avatar,
-  arribaIzquierda = <div></div>,
-  arribaDerecha = <div></div>,
-  abajoIzquierda = <div></div>,
-  abajoDerecha = <div></div>,
+  arribaIzquierda,
+  arribaDerecha,
+  abajoIzquierda,
+  abajoDerecha,
   expansion,
 }: QTarjetaGenericaProps) => {
   const [expandida, setExpandida] = useState(mostrarTodo);
@@ -26,18 +26,27 @@ export const QTarjetaGenerica = ({
   return (
     <article className="qtarjeta-generica" data-expandida={expandida}>
       <section className="qtarjeta-generica-principal">
-        {avatar && (
+        {/* {avatar && (
           <section className="qtarjeta-generica-avatar">{avatar}</section>
-        )}
+        )} */}
+        {avatar}
 
         <section
           className="qtarjeta-generica-grid"
           aria-label="Campos prioritarios"
         >
-          {arribaIzquierda ?? <div></div>}
-          {arribaDerecha ?? <div></div>}
-          {abajoIzquierda ?? <div></div>}
-          {abajoDerecha ?? <div></div>}
+          {(arribaIzquierda || arribaDerecha) && (
+            <section className="qtarjeta-generica-grid-arriba">
+              <div>{arribaIzquierda}</div>
+              <div>{arribaDerecha}</div>
+            </section>
+          )}
+          {(abajoIzquierda || abajoDerecha) && (
+            <section className="qtarjeta-generica-grid-abajo">
+              <div>{abajoIzquierda}</div>
+              <div>{abajoDerecha}</div>
+            </section>
+          )}
         </section>
       </section>
 
