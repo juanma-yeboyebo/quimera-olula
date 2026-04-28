0491c5924516c736d415fbe936a3f3c11c423179
Javier-Valero-yeboyebo <jvaola@gmail.com>
2026-04-28 09:48:05 +0200

    refact(InformePedidosXAgente): Corregida alineación de elementos.

--- legacy/extensions/sanhigia/devol_pedidos/views/InformePedidosXAgente/InformePedidosXAgente.ui.jsx
+++ legacy/extensions/sanhigia/devol_pedidos/views/InformePedidosXAgente/InformePedidosXAgente.ui.jsx
@@ -1,7 +1,7 @@
-import { Box, Button, Field, Grid, Icon, QBox } from "@quimera/comps";
 import { Agente } from "@quimera-extension/base-ventas";
-import Quimera, { PropValidation, useStateValue } from "quimera";
-import React, { useEffect } from "react";
+import { Box, Button, Field, Grid, Icon, QBox } from "@quimera/comps";
+import Quimera, { useStateValue } from "quimera";
+import { useEffect } from "react";
 
 import initialData from "./initial-data";
 
@@ -46,14 +46,17 @@ function InformePedidosXAgente({ useStyles }) {
               />
             </Box>
           </Grid>
-          <Grid size={6}>
-            <Field.Date id="filtro.fechaDesde" label="Fecha desde" className={classes.field} />
+          <Grid container spacing={1}>
+            <Grid size={6}>
+              <Field.Date id="filtro.fechaDesde" label="Fecha desde" className={classes.field} />
+            </Grid>
+            <Grid size={6}>
+              <Field.Date id="filtro.fechaHasta" label="Fecha hasta" className={classes.field} />
+            </Grid>
           </Grid>
-          <Grid size={6}>
-            <Field.Date id="filtro.fechaHasta" label="Fecha hasta" className={classes.field} />
-          </Grid>
+
         </Grid>
-        <Box display="flex" justifyContent="space-around" mt={1}>
+        <Box display="flex" justifyContent="space-around" mt={2}>
           <Button
             id="cargarDatos"
             text="Descargar"
